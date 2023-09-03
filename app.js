3c742903567c32010695d6454178a10a7ac577b8
Krushna1621 <shindekrushna825@gmail.com>
2023-09-03 11:14:03 +0530

    succefully added routing

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const http=require('http')
+const http=require('http');
+const fs=require('fs');
 const server = http.createServer((req, res) => {
-    console.log(req);
-    console.log('Krushnkant shinde')
+    const url=req.url;
+    const method=req.method;
+    if(url==='/'){
+        res.write('<html>')
+        res.write('<head><title>Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>')
+        return res.end()
+    }
+    if(url==='/message' && method==='POST'){
+            fs.writeFileSync('message.txt','DUMMY')
+            res.statusCode=302;
+            res.setHeader('Location','/')
+            return res.end()
+    }
+    
   });
 server.listen(4000)
