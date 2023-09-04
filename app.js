60e694e549bcd3df0315aff63f6093cd108e378c
Krushna1621 <shindekrushna825@gmail.com>
2023-09-04 19:04:58 +0530

    routes added

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const http=require('http');
-const routes=require('./routes')
-const server = http.createServer(routes);
+const express=require('express')
+
+const app=express()
+app.use('/add-product',(req,res,next)=>{
+    console.log('another middlware')
+    res.send('<h1>Add Product added</h1>')
+})
+
+app.use('/',(req,res,next)=>{
+    console.log('another middlware')
+    res.send('<h1>Krushnkant Shinde</h1>')
+})
+
+const server = http.createServer(app);
 server.listen(4000)
