89bf94671fd6cb0d60a879ace3a31d9ce2daafdc
Krushna1621 <shindekrushna825@gmail.com>
2023-09-08 12:18:31 +0530

    done

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const http=require('http');
 const express=require('express')
+const bodyParser=require('body-parser')
 
 const app=express()
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use((req,res,next)=>{
+     console.log('This is always runs!')
+     next()
+})
 app.use('/add-product',(req,res,next)=>{
     console.log('another middlware')
-    res.send('<h1>Add Product added</h1>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+})
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
 })
 
 app.use('/',(req,res,next)=>{
@@ -12,5 +24,4 @@ app.use('/',(req,res,next)=>{
     res.send('<h1>Krushnkant Shinde</h1>')
 })
 
-const server = http.createServer(app);
-server.listen(4000)
+app.listen(4000)
